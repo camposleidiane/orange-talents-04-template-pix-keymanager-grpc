f4475b5079b79a456cae7830d53af10841dcd8ef
Leidiane Campos <leidiane.campos@zup.com.br>
2021-06-22 11:24:58 -0300

    001 - Setup do Projeto

---
+++ src/main/kotlin/br/com/orangetalents/Application.kt
@@ -0,0 +1,10 @@
+package br.com.orangetalents
+
+import io.micronaut.runtime.Micronaut.*
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("br.com.orangetalents")
+		.start()
+}
+
